65e83ab02d81ee09d870d0488bad45fd7bbe06a7
Md-Ruhul-Amin-Rony <adiar1234@yahoo.com>
2022-11-09 12:52:22 +0100

    ch

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [".Net Developer", "Fullstack Developer", "Blogger", "Backend Developer", "Freelancer"],
+        strings: [".Net Developer", "F-stack Developer", "Blogger", "B-end Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [".Net Developer", "Fullstack Developer", "Blogger", "Backend Developer", "Freelancer"],
+        strings: [".Net Developer", "F-stack Developer", "Blogger", "B-end Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
