36066209db41008c69b08a04148b2a2f3f4c05ee
Md-Ruhul-Amin-Rony <adiar1234@yahoo.com>
2022-11-11 13:48:36 +0100

    resume data from json

---
+++ jsonData.js
@@ -0,0 +1,138 @@
+const jsonData = {
+    "basics": {
+    "name": "Md Ruhul Amin",
+    "label": ".NET Fullstack Developer",
+    "ci":"Contact Info",
+    "image": "images/pic-final.png",
+    "email1": "adiar1234@yahoo.com",
+    "email2": "mdruhul.amin@chasacademy.se",
+    "phone": "0723980098",
+    "url": "https://md-ruhul-amin-rony.github.io/portfolio-final/",
+    "fb":"https://www.facebook.com/adiar.h.adi",
+    "lid":"https://www.linkedin.com/in/md-ruhul-amin-104140128/",
+    "gih":"https://github.com/Md-Ruhul-Amin-Rony",
+    "ads":"Solhagavägen 30, 14331 Vårby",
+    "am":"About Me",
+    "para":"Som person är jag flexibel, social och alltid nyfiken och öppen för nya möjligheter. Jag har lätt för att samarbeta och under tidigare anställningar har jag alltid fått bra feedback från chefer och kollegor.Som person jag är flexibel, social, nyfiken och öppen för nya möjligheter. Jag har ett positivt förhållningssätt i samarbetet med andra människor. Under min tidigare arbetsplats fick jag väldigt bra feedback från min chef samt mina kollegor. Jag har lärt mig förhandlingsteknik och älskar att lösa problem och skapa nya program för att göra livet lättare och ta nya utmaningar som behövs arbeta inom Tech-branschen.",
+    
+    
+    "location": {
+        "address": "Solhagavägen 30",
+        "postalCode": "143 31",
+        "city": "Vårby",
+        "countryCode": "SE",
+        "region": "Stockholm"
+    },
+    
+    },
+    "work": [
+    
+    {
+        "name": "Tornado AB",
+        "position": "Andra Assistant",
+        "url": "https://McDonalds.se",
+        "startDate": "2010",
+        "endDate": "2016",
+        "summary": "I worked as a second assistant and was responsible for schedule making for my team,controlling the team while working, making order and take care of customers opinion and many more",
+        "highlights": [
+        "Started the company"
+        ]
+    },
+    {
+        "name": "Egen Företagare",
+        "position": "Director",
+        "url": "https://facebrotherhb.se",
+        "startDate": "2015",
+        "endDate": "2019",
+        "summary": "I have two different company where i use to sell products online and also have a physical shop. I used to do everything from making schedule for employees, take care of all the orders receiving through our website,take care of stock and also helping with deliveries. I have also diploma from ATG & Svenska Spel and diploma from Mcdonald's business school about assistant shift management",
+        "highlights": [
+        "Started the company"
+        ]
+    },
+    {
+        "name": "KGH CUSTOMS",
+        "position": "Customs Consultant",
+        "url": "https://kghcustoms.se",
+        "startDate": "2020",
+        "endDate": "2021",
+        "summary": "I worked as a customs consultant where i use to give advice to different company about their problems.I use to apply for different permission to the Tax Office for my clients so that my client can reduce their cost and can follow all the rules and regulations for export and import. ",
+        "highlights": [
+        "Started the company"
+        ]
+    }
+    ],
+    "References": [
+    {
+        "name": "MIMIE BUSKAS",
+        "position": "Product Manager",
+        "url": "https://organization.com/",
+        "Mobil": "0702175446",
+        "email": "mimie.buskas@gmail.com"
+    },
+    {
+        "name": "STEFAN BERGQUIVIST",
+        "position": "Station Manager",
+        "url": "https://hertz.se/",
+        "Mobil": "0736252852",
+        "email": "stefan.berquivist@hetz.se.com"
+    }
+    ],
+    "education": [
+    {
+        "institution": "CHAS ACADEMY",
+        "url": "https://chasacademy.se/",
+        "area": ".NET FULLSTACK DEVELOPER",
+        "studyType": "Vocational",
+        "startDate": "2022",
+        "endDate": "2024",
+        "description": "I have studied .NET Fullstack Developer programme where i have learned about Frontend, Backend  and Database system. In Frontend system i have learned about HTML,CSS and JavaScript and responsiveness of website and also how to fetch data from JSON file and using API. I have learned different programming languages such as Javascript,C# and still learning other languages as well. I have learned different database system, architecture and cloud system and still learning ..."
+    },
+    {
+        "institution": "TUC YRKESHÖGSKOLA",
+        "url": "https://www.tucsweden.se/",
+        "area": "Customs Complaince Specialist",
+        "studyType": "Vocational",
+        "startDate": "2020",
+        "endDate": "2022",
+        "description": "In this programme i have studied different customs procedure for export and import. How to declare customs code for different products and calculating tax and tull of these products. Even how to apply for different permission to customs office was also part of our education..."
+    },
+    {
+        "institution": "DIH",
+        "url": "http://www.diuedubdpc.com/",
+        "area": "CSE",
+        "studyType": "Bachelors",
+        "startDate": "2006",
+        "endDate": "2009",
+        "description": "I have studied computer science and engineering where i have learned all the basics about computer and tech-world."
+    }
+    ],
+    "awards": [
+    {
+        "title": "50% TUTION WAIVER",
+        "date": "2006-11-01",
+        "awarder": "North South University",
+        "summary": "Because of good results on previous studies."
+    },
+    {
+        "title": "EMPLOYEE OF THE YEAR",
+        "date": "2011-12-12",
+        "awarder": "Tornado AB",
+        "summary": "Beacause of the best performance at work ."
+    }
+    ],
+    "skills": [
+    {
+        "name": "Fullstack Developer",
+        "level": "Master",
+        "keywords": [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "C#",
+        "Database",
+        ".Net Framework",
+        "MVC architecture"
+        ]
+    }
+    ],
+}
